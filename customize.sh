f34395ff5d5f41b3d7690e957fa394cd0b119ada
chefyhalf <106939706+chefyhalf@users.noreply.github.com>
2026-07-17 12:32:25 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -2,4 +2,5 @@
 # 仅添加 Passwall 及其依赖源
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall;luci' >>feeds.conf.default
-echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki;main' >>feeds.conf.default
+echo 'src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' >> feeds.conf.default
+
